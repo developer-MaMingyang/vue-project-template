20e0d93e1b4b2c17f90b976b8e8f415e56513c8f
ma <poet0@vip.qq.com>
2018-11-08 15:48:11 +0800

    完善。加入进度条。

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import 'normalize.css';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 
@@ -12,6 +14,16 @@ import router from './router';
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
+
+router.beforeEach((to, from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach(() => {
+  NProgress.done();
+  window.scrollTo(0, 0); // 每次进入新view后，返回顶部
+});
 
 /* eslint-disable no-new */
 new Vue({
